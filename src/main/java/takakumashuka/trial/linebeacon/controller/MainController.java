3c3ab2df6a1b6e04ba385751b87ad481eeadedd9
sejour <shuka.takakuma@gmail.com>
2016-10-24 00:17:38 +0900

    refactor message handler

--- src/main/java/takakumashuka/trial/linebeacon/controller/MainController.java
+++ src/main/java/takakumashuka/trial/linebeacon/controller/MainController.java
@@ -6,9 +6,13 @@ import com.linecorp.bot.model.event.BeaconEvent;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.beacon.BeaconContent;
+import com.linecorp.bot.model.event.message.ImageMessageContent;
 import com.linecorp.bot.model.event.message.MessageContent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.ImageMessage;
+import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.profile.UserProfileResponse;
 import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.LineBotMessages;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @EnableAutoConfiguration
@@ -37,26 +42,51 @@ public class MainController {
     public void callback(@LineBotMessages List<Event> events) throws IOException {
         for (Event event : events) {
             System.out.println("event: " + event);
+            ReplyMessage replyMessage = null;
+
             if (event instanceof MessageEvent) {
-                MessageContent message = ((MessageEvent) event).getMessage();
-                if (message instanceof TextMessageContent) {
-                    System.out.println("Sending reply message");
-                    TextMessageContent textMessageContent = (TextMessageContent) message;
-                    BotApiResponse apiResponse = lineMessagingService.replyMessage(
-                            new ReplyMessage(((MessageEvent) event).getReplyToken(), new TextMessage(textMessageContent.getText()))
-                    ).execute().body();
-                    System.out.println("Sent messages: " + apiResponse);
-                }
+                replyMessage = handleMessageEvent((MessageEvent) event);
             }
             else if (event instanceof BeaconEvent) {
-                BeaconEvent beaconEvent = (BeaconEvent) event;
-                BeaconContent beacon = beaconEvent.getBeacon();
-                BotApiResponse apiResponse = lineMessagingService.replyMessage(
-                        new ReplyMessage(beaconEvent.getReplyToken(), new TextMessage("Beacon受信!! [" + beacon + "]"))
-                ).execute().body();
-                System.out.println("Received beacon: " + apiResponse);
+                replyMessage = handleBeaconEvent((BeaconEvent) event);
+            }
+
+            if (replyMessage != null) {
+                BotApiResponse apiResponse = lineMessagingService.replyMessage(replyMessage).execute().body();
+                System.out.println("Sent messages: " + apiResponse);
             }
         }
+    }
+
+    private ReplyMessage handleMessageEvent(MessageEvent event) throws IOException {
+        Message replyMessage = null;
+        MessageContent message = event.getMessage();
+
+        if (message instanceof TextMessageContent) {
+            TextMessageContent textMessageContent = (TextMessageContent) message;
+            String text = textMessageContent.getText();
+
+            if (text.equals("user")) {
+                UserProfileResponse profile = lineMessagingService.getProfile(event.getSource().getUserId()).execute().body();
+                text = String.format("userId:%s, displayName:%s", profile.getUserId(), profile.getDisplayName());
+            }
+
+            replyMessage = new TextMessage(text);
+        }
+        else if (message instanceof ImageMessageContent) {
+            replyMessage = new TextMessage("画像を受け取りました。");
+        }
+        else {
+            replyMessage = new TextMessage("[SYSTEM] received MessageEvent");
+        }
+
+        return new ReplyMessage(event.getReplyToken(), replyMessage);
+    }
+
+    private ReplyMessage handleBeaconEvent(BeaconEvent event) {
+        BeaconEvent beaconEvent = (BeaconEvent) event;
+        BeaconContent beacon = beaconEvent.getBeacon();
+        return new ReplyMessage(beaconEvent.getReplyToken(), new TextMessage("Beacon受信!! [" + beacon + "]"));
     }
 
 }
